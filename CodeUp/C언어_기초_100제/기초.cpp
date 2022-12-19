66f540276418d42c806cea2e5b92c67e97a1ebe8
HWI <dlwognl99@naver.com>
2022-12-19 11:58:44 +0900

    1025번

--- CodeUp/C언어_기초_100제/기초.cpp
+++ CodeUp/C언어_기초_100제/기초.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main()
 {
-    string a;
-    getline(cin, a);
-    cout << a;
+    int num;
+    cin >> num;
+    int a = num / 10000;
+    int b = (num - a * 10000) / 1000;
+    int c = (num - a * 10000 - b * 1000) / 100;
+    int d = (num - a * 10000 - b * 1000 - c * 100) / 10;
+    int e = (num - a * 10000 - b * 1000 - c * 100) % 10;
+    cout << "[" << a * 10000 << "]\n";
+    cout << "[" << b * 1000 << "]\n";
+    cout << "[" << c * 100 << "]\n";
+    cout << "[" << d * 10 << "]\n";
+    cout << "[" << e << "]\n";
 }
