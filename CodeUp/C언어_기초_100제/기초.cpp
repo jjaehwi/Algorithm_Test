aa3a88f048e8980378e780077c1928359a26b601
HWI <dlwognl99@naver.com>
2022-12-19 15:20:51 +0900

    1048번

--- CodeUp/C언어_기초_100제/기초.cpp
+++ CodeUp/C언어_기초_100제/기초.cpp
@@ -3,14 +3,7 @@ using namespace std;
 
 int main()
 {
-    long long a, b;
-    cout << fixed;
-    cout.precision(2);
+    int a, b;
     cin >> a >> b;
-    cout << a + b << endl;
-    cout << a - b << endl;
-    cout << a * b << endl;
-    cout << a / b << endl;
-    cout << a % b << endl;
-    cout << (float)a / b << endl;
+    cout << (a << b);
 }
