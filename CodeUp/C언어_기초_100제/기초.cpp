1a9c573d790e66fb5f12b690a6992a540efedfc6
HWI <dlwognl99@naver.com>
2022-12-19 12:18:12 +0900

    1029번

--- CodeUp/C언어_기초_100제/기초.cpp
+++ CodeUp/C언어_기초_100제/기초.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-    int num;
-    cin >> num;
-    int a = num / 10000;
-    int b = (num - a * 10000) / 1000;
-    int c = (num - a * 10000 - b * 1000) / 100;
-    int d = (num - a * 10000 - b * 1000 - c * 100) / 10;
-    int e = (num - a * 10000 - b * 1000 - c * 100) % 10;
-    cout << "[" << a * 10000 << "]\n";
-    cout << "[" << b * 1000 << "]\n";
-    cout << "[" << c * 100 << "]\n";
-    cout << "[" << d * 10 << "]\n";
-    cout << "[" << e << "]\n";
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(11);
+    cout << a;
 }
