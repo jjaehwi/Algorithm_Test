b801345f0fc90a018ffb0f4b831619a01bfbfd7c
HWI <dlwognl99@naver.com>
2022-12-20 23:44:03 +0900

    CodeUp: 1097번

--- CodeUp/C언어_기초_100제/기초.cpp
+++ CodeUp/C언어_기초_100제/기초.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int arr[20][20];
+
 int main()
 {
-    long long h, b, c, s;
-    cin >> h >> b >> c >> s;
-    cout << fixed;
-    cout.precision(1);
-    cout << (float)(h * b * c * s) / 8 / 1024 / 1024 << " MB" << endl; // bit -> byte -> KB -> MB
+    for (int i = 1; i < 20; i++)
+    {
+        for (int j = 1; j < 20; j++)
+            cin >> arr[i][j];
+    }
+
+    int n;
+    cin >> n;
+
+    int x, y;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        for (int j = 1; j < 20; j++)
+            arr[x][j] = !arr[x][j];
+        for (int j = 1; j < 20; j++)
+            arr[j][y] = !arr[j][y];
+    }
+
+    for (int i = 1; i < 20; i++)
+    {
+        for (int j = 1; j < 20; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
 }
