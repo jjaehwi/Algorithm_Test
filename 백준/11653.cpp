1db1104d555d29f415a4ada5c01eece33858f404
HWI <dlwognl99@naver.com>
2023-01-12 16:17:25 +0900

    백준: 11653번

---
+++ 백준/11653.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define fastio ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL)
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    fastio;
+    cin >> N;
+    if (N == 1)
+        return 0;
+    for (int i = 2; i <= N; i++)
+        while (N % i == 0)
+        {
+            cout << i << "\n";
+            N /= i;
+        }
+}
