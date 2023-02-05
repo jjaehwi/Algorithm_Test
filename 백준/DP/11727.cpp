714140ea418298a2fd67cab362507f0c6ddd9b93
HWI <dlwognl99@naver.com>
2023-02-05 16:34:58 +0900

    백준: 11727번 vector 풀이 추가

--- 백준/DP/11727.cpp
+++ 백준/DP/11727.cpp
@@ -1,26 +1,18 @@
 // 2 x n 타일링 2 S3
 // dp
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 int main()
 {
-
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
+    fastio;
     int n;
     cin >> n;
-    int dp[1000];
-    dp[0] = 1;
-    dp[1] = 3;
-    for (int i = 2; i < n; i++)
-    {
+    vector<int> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 3;
+    for (int i = 3; i <= n; i++)
         dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
-    }
-    cout << dp[n - 1];
-
-    return 0;
+    cout << dp[n] % 10007;
 }
