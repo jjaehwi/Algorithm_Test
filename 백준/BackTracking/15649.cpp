0c92935dcbaed3d87b92a140c39886bcecfc8f6f
HWI <dlwognl99@naver.com>
2023-03-16 11:35:43 +0900

    백준: 15649번 next_permutation 코드 추가

--- 백준/BackTracking/15649.cpp
+++ 백준/BackTracking/15649.cpp
@@ -1,6 +1,7 @@
 // N과 M (1) S3
 // 백트래킹
-// 중복 순열, next_permutation..?
+// 중복 순열, next_permutation
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -41,3 +42,62 @@ int main()
 
     permutation(0);
 }
+
+/*
+// 중복 순열
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N, M;
+int main()
+{
+    cin >> N >> M;
+    vector<int> a;
+    for (int i = 1; i <= N; i++)
+        a.push_back(i);
+    do
+    {
+        for (int i = 0; i < M; i++)
+            cout << a[i] << " ";
+        cout << "\n";
+        reverse(a.begin() + M, a.end());
+    } while (next_permutation(a.begin(), a.end()));
+}
+// https://dlog0518.tistory.com/54
+*/
+
+/*
+// 중복 없는 순열
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N, M;
+int main()
+{
+    cin >> N >> M;
+    vector<int> a(N, 1);
+    for (int i = 0; i < M; i++)
+        a[i] = 0;
+    for (int i = 0; i < N; i++)
+        cout << a[i] << " ";
+    cout << "\n";
+    do
+    {
+        for (int i = 0; i < N; i++)
+        {
+            if (a[i] == 0)
+                cout << i + 1 << " ";
+        }
+        cout << "\n";
+    } while (next_permutation(a.begin(), a.begin() + N));
+}
+
+12
+13
+14
+23
+24
+34
+*/
