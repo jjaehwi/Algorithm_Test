59263170d4671abec5eba3ba5063d6a4ab664cee
HWI <dlwognl99@naver.com>
2023-04-02 22:15:22 +0900

    백준: 12865번

---
+++ 백준/DP/12865.cpp
@@ -0,0 +1,35 @@
+// 평범한 배낭 G5
+// dp, 배낭 문제
+// 0 - 1 knapsack 점화식 : DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + V[i]);
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+int DP[101][100001];
+int W[101];
+int V[101];
+
+// 점화식 max(DP[i-1][j], DP[i-1][j-W[i]])
+
+int main()
+{
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++)
+        cin >> W[i] >> V[i];
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= K; j++)
+        {
+
+            if (j - W[i] >= 0)
+                DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + V[i]);
+            else
+                DP[i][j] = DP[i - 1][j];
+        }
+    }
+
+    cout << DP[N][K];
+}
