fcd8ac430924f18ce3e8668535b1ac167f4ad4df
HWI <dlwognl99@naver.com>
2023-04-03 22:24:19 +0900

    백준: 6593번 수정

--- 백준/DFS&BFS/6593.cpp
+++ 백준/DFS&BFS/6593.cpp
@@ -3,6 +3,98 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <memory.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define MAX_SIZE 32
+int L, R, C;
+string str;
+char map[MAX_SIZE][MAX_SIZE][MAX_SIZE];
+bool visited[MAX_SIZE][MAX_SIZE][MAX_SIZE];
+struct building
+{
+    int x;
+    int y;
+    int z;
+    int time;
+};
+
+int dx[] = {1, -1, 0, 0, 0, 0};
+int dy[] = {0, 0, 1, -1, 0, 0};
+int dz[] = {0, 0, 0, 0, 1, -1};
+
+int stateVal;
+
+int bfs(int start_x, int start_y, int start_z, int start_time)
+{
+    queue<building> q;
+    q.push({start_x, start_y, start_z, start_time});
+    while (!q.empty())
+    {
+        building current = q.front();
+        q.pop();
+        for (int i = 0; i < 6; i++)
+        {
+            int nx = current.x + dx[i];
+            int ny = current.y + dy[i];
+            int nz = current.z + dz[i];
+            if (nx < 0 || nx > L - 1 || ny < 0 || ny > R - 1 || nz < 0 || nz > C - 1)
+                continue;
+            if (map[nx][ny][nz] == 'E')
+            {
+                cout << "Escaped in " << current.time + 1 << " minute(s).\n";
+                return 1;
+            }
+            if (map[nx][ny][nz] == '.' && visited[nx][ny][nz] == false)
+            {
+                q.push({nx, ny, nz, current.time + 1});
+                visited[nx][ny][nz] = true;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    fastio;
+    while (1)
+    {
+        int start_x, start_y, start_z;
+        stateVal = 0;
+        memset(visited, false, sizeof(visited));
+        cin >> L >> R >> C;
+        if (L == 0 or R == 0 or C == 0)
+            break;
+
+        for (int i = 0; i < L; i++)
+        {
+            for (int j = 0; j < R; j++)
+            {
+                cin >> str;
+                for (int k = 0; k < C; k++)
+                {
+                    map[i][j][k] = str[k];
+                    if (map[i][j][k] == 'S')
+                    {
+                        start_x = i;
+                        start_y = j;
+                        start_z = k;
+                        visited[i][j][k] = true;
+                    }
+                }
+            }
+        }
+        stateVal = bfs(start_x, start_y, start_z, 0);
+        if (stateVal == -1)
+            cout << "Trapped!\n";
+    }
+}
+
+/*
+#include <iostream>
+#include <queue>
+#include <string>
 
 using namespace std;
 
@@ -92,3 +184,4 @@ int main()
         }
     }
 }
+*/
