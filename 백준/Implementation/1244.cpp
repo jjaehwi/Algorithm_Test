242ff2854dda3bbd1acd9640d99b048781f025dc
HWI <dlwognl99@naver.com>
2023-04-15 19:24:38 +0900

    백준: 1244번 수정

--- 백준/Implementation/1244.cpp
+++ 백준/Implementation/1244.cpp
@@ -1,40 +1,43 @@
+// 스위치 켜고 끄기 S4
+// 구현, 시뮬레이션
 #include <iostream>
 using namespace std;
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define MAX_SIZE 101
+
+int n, arr[MAX_SIZE], student, g, y, i, k;
 
 int main()
 {
-    int n, arr[101], std, g, y, i, k;
+    fastio;
+    cin >> n;
 
-    cin >> n; // 스위치의 개수 입력 받기
-
+    // 스위치의 상태 입력 받기
     for (i = 1; i <= n; i++)
-    {
-        cin >> arr[i]; // 스위치의 상태 입력 받기
-    }
+        cin >> arr[i];
 
-    cin >> std; // 학생 수 입력 받기
-    for (k = 0; k < std; k++)
+    cin >> student;
+    for (k = 0; k < student; k++)
     {
-        cin >> g >> y; // 학생의 성별, 번호 입력
+        // 학생의 성별, 번호 입력
+        // 남학생 1, 여학생 2
+        cin >> g >> y;
 
         if (g == 1) // 남학생일 때
         {
             for (i = 1; i <= n; i++)
-            {
                 if (i % y == 0)       // 배수일 때
                     arr[i] = !arr[i]; // 스위치의 상태를 바꾼다.
-            }
         }
-
         else // 여학생일 때
         {
             arr[y] = !arr[y];                          // 우선 자기자신(중심)의 상태를 바꾼다.
-            for (i = 1; arr[y + i] == arr[y - i]; i++) // <x o x> || <x가 같다면 실행>
+            for (i = 1; arr[y + i] == arr[y - i]; i++) // 대칭이라면 실행
             {
                 if (y + i > n || y - i < 1) // 인덱스가 벗어나면 멈춘다.
                     break;
-                arr[y + i] = !arr[y + i]; // 뒤집
-                arr[y - i] = !arr[y - i]; // 뒤집
+                arr[y + i] = !arr[y + i]; // 상태 바꾸기
+                arr[y - i] = !arr[y - i]; // 상태 바꾸기
             }
         }
     }
@@ -42,7 +45,7 @@ int main()
     for (i = 1; i <= n; i++)
     {
         cout << arr[i] << " ";
-        if (i % 20 == 0) // 20개의 숫자를 출력했다면 줄바꾸기
+        if (i % 20 == 0) // 한 줄에 20개씩 출력
             cout << "\n";
     }
 }
