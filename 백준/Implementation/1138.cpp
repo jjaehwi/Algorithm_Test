d17e10800457e1112f825a47b64fc0c2a36d811c
HWI <dlwognl99@naver.com>
2023-05-06 16:09:12 +0900

    백준: 1138번

---
+++ 백준/Implementation/1138.cpp
@@ -0,0 +1,32 @@
+// 한 줄로 서기 S2
+// 구현
+#include <iostream>
+using namespace std;
+int n;
+int a[11], ans[11];
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i] == cnt)
+            {
+                if (!ans[j])
+                {
+                    ans[j] = i + 1;
+                    break;
+                }
+            }
+            else if (!ans[j])
+                cnt++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << ans[i] << ' ';
+}
