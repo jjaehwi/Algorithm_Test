14f78b48394d2f09811a602e76eae51bd14166e7
HWI <dlwognl99@naver.com>
2023-06-11 20:51:04 +0900

    백준py: 5635번

---
+++ 백준_py/5635.py
@@ -0,0 +1,13 @@
+# 생일 S5
+# 구현, 문자열, 정렬
+import sys
+input = sys.stdin.readline
+
+lst = []
+for _ in range(int(input())):
+    n, d, m, y = input().rstrip().split()
+    d, m, y = map(int, (d, m, y))
+    lst.append((y, m, d, n))
+lst.sort()
+print(lst[-1][3])
+print(lst[0][3])
