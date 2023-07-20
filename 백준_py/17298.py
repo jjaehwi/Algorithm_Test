90c49c3092293b819235d7a4d17775730f13319b
HWI <dlwognl99@naver.com>
2023-07-20 17:08:56 +0900

    백준py: 17298번

---
+++ 백준_py/17298.py
@@ -0,0 +1,16 @@
+# 오큰수 G4
+# 자료 구조, 스택
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+stack = []
+answer = [-1 for i in range(n)]
+
+for i in range(len(arr)):
+    while stack and arr[stack[-1]] < arr[i]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+print(*answer)
