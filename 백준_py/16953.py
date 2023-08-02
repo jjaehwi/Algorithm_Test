917fe82845f48604d31198d6cd48ee2050d931c1
HWI <dlwognl99@naver.com>
2023-08-02 15:41:11 +0900

    백준py: 16953번

---
+++ 백준_py/16953.py
@@ -0,0 +1,17 @@
+# A → B S2
+# 그리디, 그래프 이론
+a, b = map(int, input().split())
+r = 1
+while b != a:
+    r += 1
+    temp = b
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b //= 2
+
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)
